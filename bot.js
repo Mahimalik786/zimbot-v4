9907eb81e9c28423f6f0ac26b3d9aac6a55f417f
Mahimalik786 <113202808+Mahimalik786@users.noreply.github.com>
2022-09-09 22:43:35 +0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -33,17 +33,17 @@ global.APIKeys = {
 }
 
 //settings
-global.owner = ['27634090203',''] //owner number +27634090203
-global.wame = ['27634090203'] 
-global.pemilik = ['27634090203'] //owner number +27634090203
-global.premium = ['27634090203'] //premium number +27634090203
-global.pengguna = 'Drips' //username
+global.owner = ['923482557718',''] //owner number +923482557718
+global.wame = ['923482557718'] 
+global.pemilik = ['923482557718'] //owner number +923482557718
+global.premium = ['923482557718'] //premium number +923482557718
+global.pengguna = 'MahiMalik' //username
 global.footer = 'ZIM BOT INC'
 global.botnma = 'ZIM BOT INC' //bot name
 global.botname = 'ZIM BOT V4'
-global.ownernma = 'Drips' //owner name
-global.packname = 'Drips' //sticker package name
-global.author = 'By Drips Memes' //sticker author name
+global.ownernma = 'MahiMalik' //owner name
+global.packname = 'MahiMalik' //sticker package name
+global.author = 'By Mahi Memes' //sticker author name
 global.sessionName = 'session' //session name
 global.prefa = ['#','!','/',''] //prefix
 global.sp = '🔵' //sp
